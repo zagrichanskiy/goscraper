cace81ca91455e10003e97df885cdbef999f3581
Oleg Zagrichanskiy <zagrichanskiy@gmail.com>
2018-10-05 00:28:49 +0300

    Use full links to the files
    Signed-off-by: Oleg Zagrichanskiy <zagrichanskiy@gmail.com>

--- main.go
+++ main.go
@@ -1,77 +1,67 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"path/filepath"
-)
-
 const (
-	// DirName folder in the $HOME
-	DirName = ".goscraper"
-	// ConfigName name of config
-	ConfigName = "config.json"
-	// DefaultURL to look up builds
-	DefaultURL = "http://dehil-ae03.debads.europe.delphiauto.net:82/platforms/csp/csp/"
+	// RootURL to look up builds.
+	RootURL = "http://dehil-ae03.debads.europe.delphiauto.net:82/" +
+		"platforms/csp/csp/latest-release/"
+	// SdkURL url to the sdk folder.
+	SdkURL = RootURL + "sdk"
+	// SdkReg regexp for the sdk file.
+	SdkReg = `\.sh$`
+	// Blade1URL url to the blade 1
+	Blade1URL = RootURL + "csp-image-blade-i-intel-corei7-64-dom0.wic"
+	// Blade2URL url to the blade 3
+	Blade2URL = RootURL + "csp-image-blade-ii-intel-corei7-64-dom0.wic"
+	// Blade3URL url to the blade 3
+	Blade3URL = RootURL + "csp-image-blade-iii-intel-corei7-64-dom0.wic"
 )
 
-// JSONFormat data to store in config file
-type JSONFormat struct {
-	URL     string
-	Sdk     string
-	Blade1  string
-	Blade2  string
-	Blade3  string
-	Updated string
-}
+// func main() {
+// 	// Getting the web page.
+// 	fmt.Println("Openning url: ", DefaultURL)
+// 	resp, err := http.Get(DefaultURL)
+// 	if err != nil {
+// 		fmt.Println("Can't open the url: ", err)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
 
-func main() {
-	var (
-		dirPath    = filepath.Join(os.Getenv("HOME"), DirName)
-		configPath = filepath.Join(dirPath, ConfigName)
-	)
+// 	// Getting the list of urls on the page.
+// 	z := html.NewTokenizer(resp.Body)
+// 	urls := make([]string, 0)
+// 	for tt := z.Next(); tt != html.ErrorToken; tt = z.Next() {
+// 		if tt == html.StartTagToken {
+// 			if t := z.Token(); t.Data == "a" {
+// 				fillURLArray(&t, &urls)
+// 			}
+// 		}
+// 	}
 
-	if ok := checkFolder(dirPath); !ok {
-		os.Exit(1)
-	}
+// 	for _, url := range urls {
+// 		fmt.Println(" - ", url)
+// 	}
 
-	if ok := checkConfig(configPath); !ok {
-		os.Exit(1)
-	}
-}
+// 	// Finding the link to the balde3
+// 	fmt.Println(DefaultURL + getURL(Blade3Reg, urls))
+// }
 
-func checkFolder(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		fmt.Println("Directory ", path, " doesn't exist, creating...")
-		err := os.Mkdir(path, 0777)
-		if err != nil {
-			fmt.Println("Can't create directory ", path, ": ", err)
-			return false
-		}
-	}
+// func fillURLArray(t *html.Token, urls *[]string) {
+// 	for _, a := range t.Attr {
+// 		if a.Key == "href" {
+// 			*urls = append(*urls, a.Val)
+// 			break
+// 		}
+// 	}
+// }
 
-	return true
-}
-
-func checkConfig(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		fmt.Println("Config ", path, " doesn't exist, creating...")
-
-		jFile, err := os.Create(path)
-		if err != nil {
-			fmt.Println("Can't create ", path, ": ", err)
-			return false
-		}
-		defer jFile.Close()
-
-		jData, err := json.MarshalIndent(&JSONFormat{URL: DefaultURL}, "", "    ")
-		if err != nil {
-			fmt.Println("Can't format json: ", err)
-			return false
-		}
-
-		fmt.Fprintf(jFile, "%s\n", jData)
-	}
-	return true
-}
+// func getURL(reg string, urls []string) string {
+// 	var ret string
+// 	re := regexp.MustCompile(reg)
+// 	for _, url := range urls {
+// 		if re.MatchString(url) {
+// 			ret = url
+// 			break // We suppose there is only one match in array.
+// 		}
+// 	}
+// 	return ret
+// }
